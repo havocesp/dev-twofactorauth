693a2c11f5f7b33f593f2df150fb039852db0c71
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-04-04 02:21:34 +0200

    Add category scrolling and url parameter handling

--- js/main.js
+++ js/main.js
@@ -4,8 +4,23 @@ $(document).ready(function () {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('service-worker.js');
     }
+
+    if (window.location.hash && window.location.hash.indexOf('#') > - 1) {
+        $('.collapse').collapse('hide');
+        $('#'+window.location.hash.substring(1)+"-table").collapse("show");
+        $('#'+window.location.hash.substring(1)+"-mobile-table").collapse("show");
+    }
 });
 
-jQuery('.cat').click(function (e) {
-    jQuery('.collapse').collapse('hide');
+$('.cat').click(function (e) {
+});
+
+$('.cat').click(function () {
+    // Collapse all other tables.
+    $('.collapse').collapse('hide');
+
+
+    $('html, body').animate({
+        scrollTop: $(this).offset().top - 25
+    }, 1000);
 });
