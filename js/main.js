40583befad611688971fbba78cf25c0ce10ac354
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-04-04 21:03:12 +0200

    Recolor dark mode

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ $('.cat').click(function () {
 
     if (!$('#' + window.location.hash.substring(1) + "-table").hasClass('show') && !$('#' + window.location.hash.substring(1) + "-mobile-table").hasClass('show')) {
         window.location.hash = this.id;
+        $('#' + window.location.hash.substring(1) + "-table").collapse("show");
+        $('#' + window.location.hash.substring(1) + "-mobile-table").collapse("show");
 
         // Scrolling animation. Disabled for now due to glitching.
         /* $('html, body').animate({
